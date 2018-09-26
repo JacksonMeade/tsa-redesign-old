164822b551a8e4ff503ace649c37ed96abf4132a
Jackson Meade <jacksonmeade@outlook.com>
2018-09-26 13:16:21 -0400

    v1.8

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1002,3 +1002,21 @@
         }
     }
 })
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickerHold()};
+
+// Get the navbar
+var navbar = document.getElementById("sticker");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickerHold() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
